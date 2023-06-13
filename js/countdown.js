279e50b8ef1b234b2002ec3411b100cae99cd73a
Alan Cordoba <alancordoba010@gmail.com>
2023-06-13 19:29:54 -0300

    Update

--- js/countdown.js
+++ js/countdown.js
@@ -1,92 +1,77 @@
 simplyCountdown('#cuenta', {
-	year: 2023, // required
-	month: 5, // required
-	day: 17, // required
-	hours: 0, // Default is 0 [0-23] integer
-	minutes: 0, // Default is 0 [0-59] integer
-	seconds: 0, // Default is 0 [0-59] integer
-	words: { //words displayed into the countdown
+	year: 2023,
+	month: 5,
+	day: 17,
+	hours: 0,
+	minutes: 0,
+	seconds: 0,
+	words: {
 		days: 'Día',
 		hours: 'Hora',
 		minutes: 'Minuto',
 		seconds: 'Segundo',
 		pluralLetter: 's'
 	},
-	plural: true, //use plurals
-	inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
-	inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
-	// in case of inline set to false
-	enableUtc: true, //Use UTC as default
-	onEnd: function() {
-		document.getElementById('portada').classList.add('oculta');
-		return; 
-	}, //Callback on countdown end, put your own function here
-	refresh: 1000, // default refresh every 1s
-	sectionClass: 'simply-section', //section css class
-	amountClass: 'simply-amount', // amount css class
-	wordClass: 'simply-word', // word css class
+	plural: true,
+	inline: false,
+	inlineClass: 'simply-countdown-inline',
+	enableUtc: true,
+	refresh: 1000,
+	sectionClass: 'simply-section',
+	amountClass: 'simply-amount',
+	wordClass: 'simply-word',
 	zeroPad: false,
 	countUp: false
 });
 
 simplyCountdown('#cuentav2', {
-	year: 2010, // required
-	month: 3, // required
-	day: 3, // required
-	hours: 0, // Default is 0 [0-23] integer
-	minutes: 0, // Default is 0 [0-59] integer
-	seconds: 0, // Default is 0 [0-59] integer
-	words: { //words displayed into the countdown
+	year: 2010,
+	month: 3,
+	day: 3,
+	hours: 0,
+	minutes: 0,
+	seconds: 0,
+	words: {
 		days: 'Día',
 		hours: 'Hora',
 		minutes: 'Minuto',
 		seconds: 'Segundo',
 		pluralLetter: 's'
 	},
-	plural: true, //use plurals
-	inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
-	inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
-	// in case of inline set to false
-	enableUtc: true, //Use UTC as default
-	onEnd: function() {
-		document.getElementById('portada').classList.add('oculta');
-		return; 
-	}, //Callback on countdown end, put your own function here
-	refresh: 1000, // default refresh every 1s
-	sectionClass: 'simply-section', //section css class
-	amountClass: 'simply-amount', // amount css class
-	wordClass: 'simply-word', // word css class
+	plural: true,
+	inline: false,
+	inlineClass: 'simply-countdown-inline',
+	enableUtc: true,
+	refresh: 1000,
+	sectionClass: 'simply-section',
+	amountClass: 'simply-amount',
+	wordClass: 'simply-word',
 	zeroPad: false,
 	countUp: true
 });
 
 simplyCountdown('#cuentav3', {
-	year: 2012, // required
-	month: 0, // required
-	day: 0, // required
-	hours: 0, // Default is 0 [0-23] integer
-	minutes: 0, // Default is 0 [0-59] integer
-	seconds: 0, // Default is 0 [0-59] integer
-	words: { //words displayed into the countdown
+	year: 2012,
+	month: 0,
+	day: 0,
+	hours: 0,
+	minutes: 0,
+	seconds: 0,
+	words: {
 		days: 'Día',
 		hours: 'Hora',
 		minutes: 'Minuto',
 		seconds: 'Segundo',
 		pluralLetter: 's'
 	},
-	plural: true, //use plurals
-	inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
-	inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
-	// in case of inline set to false
-	enableUtc: true, //Use UTC as default
-	onEnd: function() {
-		document.getElementById('portada').classList.add('oculta');
-		return; 
-	}, //Callback on countdown end, put your own function here
-	refresh: 1000, // default refresh every 1s
-	sectionClass: 'simply-section', //section css class
-	amountClass: 'simply-amount', // amount css class
-	wordClass: 'simply-word', // word css class
+	plural: true,
+	inline: false,
+	inlineClass: 'simply-countdown-inline',
+	enableUtc: true,
+	refresh: 1000,
+	sectionClass: 'simply-section',
+	amountClass: 'simply-amount',
+	wordClass: 'simply-word',
 	zeroPad: false,
 	countUp: true
 });
